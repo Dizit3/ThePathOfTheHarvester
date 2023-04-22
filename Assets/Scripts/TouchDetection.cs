08cc2d2ac2c21389a47f621829f2355e2d78b1b7
Dizit3 <dizit3@gmail.com>
2023-04-22 16:31:56 +0300

    CheckPoint

--- Assets/Scripts/TouchDetection.cs
+++ Assets/Scripts/TouchDetection.cs
@@ -1,53 +1,53 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
 
-class TouchDetection : MonoBehaviour
-{
+//class TouchDetection : MonoBehaviour
+//{
 
-    private bool isMobile;
+//    private bool isMobile;
 
-    public static event OnTouchInput TouchEvent;
-    public delegate void OnTouchInput(bool isTouch);
+//    public static event OnTouchInput TouchEvent;
+//    public delegate void OnTouchInput(bool isTouch);
 
-    private bool isTouch = false;
+//    private bool isTouch = false;
 
-    private void Start()
-    {
-        isMobile = Application.isMobilePlatform;
-    }
+//    private void Start()
+//    {
+//        isMobile = Application.isMobilePlatform;
+//    }
 
 
 
-    private void Update()
-    {
+//    private void Update()
+//    {
 
-        if (!isMobile && TouchEvent != null)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                TouchEvent(isTouch = true);
+//        if (!isMobile && TouchEvent != null)
+//        {
+//            if (Input.GetMouseButtonDown(0))
+//            {
+//                TouchEvent(isTouch = true);
 
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                TouchEvent(isTouch = false);
+//            }
+//            else if (Input.GetMouseButtonUp(0))
+//            {
+//                TouchEvent(isTouch = false);
 
-            }
-        }
-        else if (Input.touchCount > 0 && TouchEvent != null)
-        {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                TouchEvent(isTouch = true);
+//            }
+//        }
+//        else if (Input.touchCount > 0 && TouchEvent != null)
+//        {
+//            if (Input.GetTouch(0).phase == TouchPhase.Began)
+//            {
+//                TouchEvent(isTouch = true);
 
-            }
-            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
-            {
-                TouchEvent(isTouch = false);
-            }
-        }
+//            }
+//            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+//            {
+//                TouchEvent(isTouch = false);
+//            }
+//        }
 
-    }
+//    }
 
-}
+//}
 
