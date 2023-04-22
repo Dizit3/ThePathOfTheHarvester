e1bdd64723a355deedd25dde5d801e5c7ee5fb0a
Dizit3 <dizit3@gmail.com>
2023-04-22 14:08:38 +0300

    SaveController0_1

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+
+    public static void Save(int score)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/savefile.gut";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, score);
+
+        stream.Close();
+    }
+
+    public static int Load()
+    {
+        string path = Application.persistentDataPath + "/"
+    }
+
+}
